b5fc600732f4ca341ee6e95215e064667bb145da
chihaso <cycyccq@gmail.com>
2019-09-16 22:24:36 +0900

    add create new memo func

--- app.rb
+++ app.rb
@@ -4,3 +4,23 @@ require 'sinatra/reloader'
 get '/' do
   erb :top
 end
+
+get '/show/*' do |num|
+  @num = num
+  erb :show
+end
+
+get '/new' do
+  erb :new
+end
+
+post '/create' do
+  file_numbers = Dir.glob('./memo_data/*').map { |fn|
+    fn.delete('^0-9').to_i
+  }
+  latest_number = file_numbers.max
+  File.open("./memo_data/#{latest_number + 1}", 'w', 0o0777) { |f|
+    f.puts params[:memo]
+  }
+  erb :top
+end
