2f5d3a8960e01d16d2d193acd37350e7f2810206
chihaso <cycyccq@gmail.com>
2019-09-23 15:13:03 +0900

    modify URI like /* -> /memos/:id

--- app.rb
+++ app.rb
@@ -3,19 +3,19 @@
 require 'sinatra'
 require 'sinatra/reloader'
 require 'fileutils'
+require 'securerandom'
 
 enable  :method_override
-
-def random_number_generator(n)
-  ''.dup.tap { |s| n.times { s << rand(0..9).to_s } }
-end
 
 # トップ（index）ページ
 get '/' do
   file_names = Dir.glob('./memo_data/*')
   @first_lines = []
-  file_names.each { |fn| @first_lines << File.open(fn, 'r').gets }
-  @file_numbers = file_names.map { |fn| fn.delete!('^0-9').to_i }
+  @file_ids =[]
+  file_names.each do |fn|
+    @first_lines << File.open(fn, 'r').gets
+    @file_ids << fn.delete_prefix('./memo_data/')
+  end
   erb :top
 end
 
@@ -26,34 +26,34 @@ end
 
 # 新規メモ作成
 post '/' do
-  memo_file = File.open("./memo_data/#{random_number_generator(10)}", 'w', 0o0666)
+  memo_file = File.open("./memo_data/#{SecureRandom.uuid}", 'w', 0o0666)
   memo_file.puts params[:memo]
   memo_file.close
   redirect to('/')
 end
 
 # メモ編集ページ
-get '/edit_*' do |num|
-  @num = num
+get '/memos/:id/edit' do
+  @id = params[:id]
   erb :edit
 end
 
 # メモ更新
-patch '/*' do |num|
-  memo_file = File.open("./memo_data/#{num}", 'w')
+patch '/memos/:id/edit' do
+  memo_file = File.open("./memo_data/#{params[:id]}", 'w')
   memo_file.puts params[:memo]
   memo_file.close
   redirect to('/')
 end
 
 # メモ削除
-delete '/*' do |num|
-  FileUtils.rm("./memo_data/#{num}")
+delete '/memos/:id' do
+  FileUtils.rm("./memo_data/#{params[:id]}")
   redirect to('/')
 end
 
 # メモ表示ページ
-get '/*' do |num|
-  @num = num
+get '/memos/:id' do
+  @id = params[:id]
   erb :show
 end
