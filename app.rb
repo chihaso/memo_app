639917883a52e8dc1e5e6b3408ae8fdeb5c60d59
chihaso <cycyccq@gmail.com>
2019-09-25 16:47:19 +0900

    replace ' with "

--- app.rb
+++ app.rb
@@ -1,58 +1,58 @@
 # frozen_string_literal: true
 
-require 'sinatra'
-require 'sinatra/reloader'
-require 'fileutils'
-require 'securerandom'
+require "sinatra"
+require "sinatra/reloader"
+require "fileutils"
+require "securerandom"
 
 # トップ（index）ページ
-get '/' do
-  file_names = Dir.glob('./memo_data/*')
+get "/" do
+  file_names = Dir.glob("./memo_data/*")
   @first_lines = []
   @file_ids =[]
   file_names.each do |fn|
-    @first_lines << File.open(fn, 'r') { |file| file.gets }
-    @file_ids << fn.delete_prefix('./memo_data/')
+    @first_lines << File.open(fn, "r") { |file| file.gets }
+    @file_ids << fn.delete_prefix("./memo_data/")
   end
   erb :top
 end
 
 # 新規メモ作成ページ
-get '/new' do
+get "/new" do
   erb :new
 end
 
 # 新規メモ作成
-post '/' do
-  File.open("./memo_data/#{SecureRandom.uuid}", 'w', 0o0666) do |file|
+post "/" do
+  File.open("./memo_data/#{SecureRandom.uuid}", "w", 0o0666) do |file|
     file.puts params[:memo]
   end
-  redirect to('/')
+  redirect to("/")
 end
 
 # メモ編集ページ
-get '/:id/edit' do
+get "/:id/edit" do
   @id = params[:id]
   @memo_text = File.read("./memo_data/#{@id}")
   erb :edit
 end
 
 # メモ更新
-patch '/:id' do
-  File.open("./memo_data/#{params[:id]}", 'w') do |file|
+patch "/:id" do
+  File.open("./memo_data/#{params[:id]}", "w") do |file|
     file.puts params[:memo]
   end
-  redirect to('/')
+  redirect to("/")
 end
 
 # メモ削除
-delete '/:id' do
+delete "/:id" do
   FileUtils.rm("./memo_data/#{params[:id]}")
-  redirect to('/')
+  redirect to("/")
 end
 
 # メモ表示ページ
-get '/:id' do
+get "/:id" do
   @id = params[:id]
   @memo_text = File.read("./memo_data/#{@id}").gsub("\n", "<br>")
   erb :show
